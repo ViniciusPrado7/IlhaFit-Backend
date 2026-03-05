550ee12725cdc59592cdd52ba6c5ee39e4d933f2
ViniciusPrado7 <vinicius.pradofake@gmail.com>
2026-03-05 20:48:56 -0300

    feat: DTOEstabelecimento

---
+++ src/main/java/com/example/ilhafit/dto/EstabelecimentoDTO.java
@@ -0,0 +1,56 @@
+package com.example.ilhafit.dto;
+
+import com.example.ilhafit.entity.Role;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+import java.util.List;
+
+public class EstabelecimentoDTO {
+
+    @Data
+    public static class Registro {
+        @NotBlank(message = "Nome é obrigatório")
+        private String nome;
+
+        private String nomeFantasia;
+        private String razaoSocial;
+
+        @NotBlank(message = "Email é obrigatório")
+        @Email(message = "Email deve ser válido")
+        private String email;
+
+        private String senha;
+
+        @NotBlank(message = "Telefone é obrigatório")
+        @Pattern(regexp = "\\d*", message = "Telefone deve conter apenas números")
+        private String telefone;
+
+        @NotBlank(message = "CNPJ é obrigatório")
+        private String cnpj;
+
+        private EnderecoDTO endereco;
+        private Boolean exclusivoMulheres;
+        private List<GradeAtividadeDTO> gradeAtividades;
+        private List<String> fotosUrl;
+        private String outrosAtividade;
+    }
+
+    @Data
+    public static class Resposta {
+        private Long id;
+        private String nome;
+        private String nomeFantasia;
+        private String razaoSocial;
+        private String email;
+        private String telefone;
+        private String cnpj;
+        private EnderecoDTO endereco;
+        private Boolean exclusivoMulheres;
+        private List<GradeAtividadeDTO> gradeAtividades;
+        private List<String> fotosUrl;
+        private String outrosAtividade;
+        private Role role;
+        private Double avaliacao;
+    }
+}
