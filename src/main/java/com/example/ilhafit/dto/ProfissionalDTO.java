2951a398c9bed98cfd3db0471e323faa02fbb37d
Lucas Cruz <cruzzzlucas1910@gmail.com>
2026-03-05 21:03:00 -0300

    feat: ProfissionalDto

--- src/main/java/com/example/ilhafit/dto/ProfissionalDTO.java
+++ src/main/java/com/example/ilhafit/dto/ProfissionalDTO.java
@@ -1,5 +1,54 @@
 package com.example.ilhafit.dto;
 
+import com.example.ilhafit.entity.Role;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+import java.util.List;
+
 public class ProfissionalDTO {
-    
+
+    @Data
+    public static class Registro {
+        @NotBlank(message = "Nome é obrigatório")
+        private String nome;
+
+        @NotBlank(message = "Email é obrigatório")
+        @Email(message = "Email deve ser válido")
+        private String email;
+
+        private String senha;
+
+        @NotBlank(message = "Telefone é obrigatório")
+        @Pattern(regexp = "\\d*", message = "Telefone deve conter apenas números")
+        private String telefone;
+
+        @NotBlank(message = "CPF é obrigatório")
+        private String cpf;
+
+        private String sexo;
+
+        private String especializacao;
+        private String registroCref;
+        private Boolean exclusivoMulheres;
+        private String fotoUrl;
+        private String outrosAtividade;
+    }
+
+    @Data
+    public static class Resposta {
+        private Long id;
+        private String nome;
+        private String email;
+        private String telefone;
+        private String cpf;
+        private String sexo;
+        private String especializacao;
+        private String registroCref;
+        private Boolean exclusivoMulheres;
+        private String fotoUrl;
+        private String outrosAtividade;
+        private Role role;
+        private Double avaliacao;
+    }
 }
